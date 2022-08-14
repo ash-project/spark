d76139f69c6954b5e433ad7ffaac30e946288ed3
Zach Daniel <zach@zachdaniel.dev>
2022-08-14 13:31:09 -0400

    chore: add config

---
+++ config/config.exs
@@ -0,0 +1,15 @@
+import Config
+
+if Mix.env() == :dev do
+  config :git_ops,
+    mix_project: Spark.MixProject,
+    changelog_file: "CHANGELOG.md",
+    repository_url: "https://github.com/ash-project/spark",
+    # Instructs the tool to manage your mix version in your `mix.exs` file
+    # See below for more information
+    manage_mix_version?: true,
+    # Instructs the tool to manage the version in your README.md
+    # Pass in `true` to use `"README.md"` or a string to customize
+    manage_readme_version: "README.md",
+    version_tag_prefix: "v"
+end
