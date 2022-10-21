ee80915ca3e4d0345afd64e29a6d5a2029ae39ea
Zach Daniel <zach@zachdaniel.dev>
2022-10-21 01:10:40 -0400

    improvement: remove necessity for `{:elixir_sense, ...}` to make the plugin work

--- lib/spark/elixir_sense/aliases.ex
+++ lib/spark/elixir_sense/aliases.ex
@@ -1,105 +1,103 @@
-if Code.ensure_loaded?(ElixirSense.Plugin) do
-  defmodule Spark.ElixirSense.Types do
-    @moduledoc false
+defmodule Spark.ElixirSense.Types do
+  @moduledoc false
 
-    alias ElixirSense.Core.Introspection
-    alias ElixirSense.Plugins.Util
-    alias ElixirSense.Providers.Suggestion.Matcher
+  alias ElixirSense.Core.Introspection
+  alias ElixirSense.Plugins.Util
+  alias ElixirSense.Providers.Suggestion.Matcher
 
-    def find_builtin_types(module, func, hint, cursor_context, templates \\ []) do
-      text_before = cursor_context.text_before
-      text_after = cursor_context.text_after
+  def find_builtin_types(module, func, hint, cursor_context, templates \\ []) do
+    text_before = cursor_context.text_before
+    text_after = cursor_context.text_after
 
-      actual_hint =
-        if String.ends_with?(text_before, "{" <> hint) do
-          "{" <> hint
-        else
-          hint
-        end
+    actual_hint =
+      if String.ends_with?(text_before, "{" <> hint) do
+        "{" <> hint
+      else
+        hint
+      end
 
-      with true <- Code.ensure_loaded?(module),
-           true <- :erlang.function_exported(module, func, 0) do
-        for {name, _, _} = type <- builtin_types(module, func, templates),
-            Matcher.match?(name, actual_hint) do
-          buitin_type_to_suggestion(type, module, actual_hint, text_after)
-        end
+    with true <- Code.ensure_loaded?(module),
+         true <- :erlang.function_exported(module, func, 0) do
+      for {name, _, _} = type <- builtin_types(module, func, templates),
+          apply(Matcher, :match?, [name, actual_hint]) do
+        buitin_type_to_suggestion(type, module, actual_hint, text_after)
       end
     end
+  end
 
-    defp builtin_types(module, func, templates) do
-      module
-      |> apply(func, [])
-      |> Enum.map(fn {name, value} ->
-        {inspect(name), value, nil}
-      end)
-      |> Enum.concat(templates)
+  defp builtin_types(module, func, templates) do
+    module
+    |> apply(func, [])
+    |> Enum.map(fn {name, value} ->
+      {inspect(name), value, nil}
+    end)
+    |> Enum.concat(templates)
+  end
+
+  def find_custom_types(type_module, func, hint, module_store) do
+    builtin_types = Keyword.values(apply(type_module, func, []))
+
+    for module <- module_store.by_behaviour[type_module] || [],
+        module not in builtin_types,
+        type_str = inspect(module),
+        apply(Util, :match_module?, [type_str, hint]) do
+      custom_type_to_suggestion(module, type_module, hint)
     end
+  end
 
-    def find_custom_types(type_module, func, hint, module_store) do
-      builtin_types = Keyword.values(apply(type_module, func, []))
+  defp buitin_type_to_suggestion({type, mod_or_description, snippet}, module, hint, text_after) do
+    [_, hint_prefix] = Regex.run(~r/(.*?)[\w0-9\._!\?\->]*$/, hint)
 
-      for module <- module_store.by_behaviour[type_module] || [],
-          module not in builtin_types,
-          type_str = inspect(module),
-          Util.match_module?(type_str, hint) do
-        custom_type_to_suggestion(module, type_module, hint)
+    insert_text = String.replace_prefix(type, hint_prefix, "")
+    snippet = snippet && String.replace_prefix(snippet, hint_prefix, "")
+
+    {insert_text, snippet} =
+      if String.starts_with?(text_after, "}") do
+        snippet = snippet && String.replace_suffix(snippet, "}", "")
+        insert_text = String.replace_suffix(insert_text, "}", "")
+        {insert_text, snippet}
+      else
+        {insert_text, snippet}
       end
-    end
 
-    defp buitin_type_to_suggestion({type, mod_or_description, snippet}, module, hint, text_after) do
-      [_, hint_prefix] = Regex.run(~r/(.*?)[\w0-9\._!\?\->]*$/, hint)
+    docs =
+      if is_binary(mod_or_description) do
+        mod_or_description
+      else
+        {doc, _} = apply(Introspection, :get_module_docs_summary, [mod_or_description])
+        doc
+      end
 
-      insert_text = String.replace_prefix(type, hint_prefix, "")
-      snippet = snippet && String.replace_prefix(snippet, hint_prefix, "")
+    doc = """
+    Built-in #{inspect(module)}
 
-      {insert_text, snippet} =
-        if String.starts_with?(text_after, "}") do
-          snippet = snippet && String.replace_suffix(snippet, "}", "")
-          insert_text = String.replace_suffix(insert_text, "}", "")
-          {insert_text, snippet}
-        else
-          {insert_text, snippet}
-        end
+    #{docs}
+    """
 
-      docs =
-        if is_binary(mod_or_description) do
-          mod_or_description
-        else
-          {doc, _} = Introspection.get_module_docs_summary(mod_or_description)
-          doc
-        end
+    %{
+      type: :generic,
+      kind: :type_parameter,
+      label: type,
+      insert_text: insert_text,
+      snippet: snippet,
+      detail: inspect(module),
+      documentation: doc,
+      priority: 0
+    }
+  end
 
-      doc = """
-      Built-in #{inspect(module)}
+  defp custom_type_to_suggestion(type, module, hint) do
+    type_str = inspect(type)
+    {doc, _} = apply(Introspection, :get_module_docs_summary, [type])
 
-      #{docs}
-      """
-
-      %{
-        type: :generic,
-        kind: :type_parameter,
-        label: type,
-        insert_text: insert_text,
-        snippet: snippet,
-        detail: inspect(module),
-        documentation: doc,
-        priority: 0
-      }
-    end
-
-    defp custom_type_to_suggestion(type, module, hint) do
-      type_str = inspect(type)
-      {doc, _} = Introspection.get_module_docs_summary(type)
-
-      %{
-        type: :generic,
-        kind: :type_parameter,
-        label: type_str,
-        insert_text: Util.trim_leading_for_insertion(hint, type_str),
-        detail: "Custom #{inspect(module)}",
-        documentation: doc,
-        priority: 1
-      }
-    end
+    %{
+      type: :generic,
+      kind: :type_parameter,
+      label: type_str,
+      insert_text: apply(Util, :trim_leading_for_insertion, [hint, type_str]),
+      detail: "Custom #{inspect(module)}",
+      documentation: doc,
+      priority: 1
+    }
   end
 end
--- lib/spark/elixir_sense/entity.ex
+++ lib/spark/elixir_sense/entity.ex
@@ -1,98 +1,99 @@
-if Code.ensure_loaded?(ElixirSense.Plugin) do
-  defmodule Spark.ElixirSense.Entity do
-    @moduledoc false
-    alias ElixirSense.Core.Introspection
-    alias ElixirSense.Plugins.Util
-    alias ElixirSense.Providers.Suggestion.Complete
+defmodule Spark.ElixirSense.Entity do
+  @moduledoc false
+  alias ElixirSense.Core.Introspection
+  alias ElixirSense.Plugins.Util
+  alias ElixirSense.Providers.Suggestion.Complete
 
-    def find_entities(type, hint) do
-      for {module, _} <- :code.all_loaded(),
-          type in (module.module_info(:attributes)[:spark_is] || []),
+  def find_entities(type, hint) do
+    for {module, _} <- :code.all_loaded(),
+        type in (module.module_info(:attributes)[:spark_is] || []),
+        mod_str = inspect(module),
+        apply(Util, :match_module?, [mod_str, hint]) do
+      {doc, _} = apply(Introspection, :get_module_docs_summary, [module])
+
+      %{
+        type: :generic,
+        kind: :class,
+        label: mod_str,
+        insert_text: apply(Util, :trim_leading_for_insertion, [hint, mod_str]),
+        detail: "Spark resource",
+        documentation: doc
+      }
+    end
+  end
+
+  def find_spark_behaviour_impls(behaviour, builtins, hint, module_store) do
+    builtins =
+      if builtins && !String.contains?(hint, ".") && lowercase_string?(hint) do
+        apply(Complete, :complete, [
+          to_string("#{inspect(builtins)}.#{hint}"),
+          apply(Complete.Env, :__struct__, [])
+        ])
+      else
+        []
+      end
+      |> Enum.reject(fn
+        %{name: name} when name in ["__info__", "module_info", "module_info"] ->
+          true
+
+        _ ->
+          false
+      end)
+      |> Enum.map(fn
+        %{type: :function, origin: origin, name: name, arity: arity} = completion ->
+          try do
+            {:docs_v1, _, _, _, _, _, functions} = Code.fetch_docs(Module.concat([origin]))
+
+            new_summary =
+              Enum.find_value(functions, fn
+                {{:function, func_name, func_arity}, _, _,
+                 %{
+                   "en" => docs
+                 }, _} ->
+                  if to_string(func_name) == name && func_arity == arity do
+                    docs
+                  end
+
+                _other ->
+                  false
+              end)
+
+            %{completion | summary: new_summary || completion.summary}
+          rescue
+            _e ->
+              completion
+          end
+
+        other ->
+          other
+      end)
+
+    first = behaviour |> Module.split() |> Enum.at(0)
+
+    custom =
+      for module <- module_store.by_behaviour[behaviour] || [],
           mod_str = inspect(module),
-          Util.match_module?(mod_str, hint) do
-        {doc, _} = Introspection.get_module_docs_summary(module)
+          !String.starts_with?(mod_str, "#{first}."),
+          apply(Util, :match_module?, [mod_str, hint]) do
+        {doc, _} = apply(Introspection, :get_module_docs_summary, [module])
 
         %{
           type: :generic,
           kind: :class,
           label: mod_str,
-          insert_text: Util.trim_leading_for_insertion(hint, mod_str),
-          detail: "Spark resource",
+          insert_text: apply(Util, :trim_leading_for_insertion, [hint, mod_str]),
+          detail: "#{inspect(behaviour)}",
           documentation: doc
         }
       end
-    end
 
-    def find_spark_behaviour_impls(behaviour, builtins, hint, module_store) do
-      builtins =
-        if builtins && !String.contains?(hint, ".") && lowercase_string?(hint) do
-          Complete.complete(to_string("#{inspect(builtins)}.#{hint}"), %Complete.Env{})
-        else
-          []
-        end
-        |> Enum.reject(fn
-          %{name: name} when name in ["__info__", "module_info", "module_info"] ->
-            true
+    builtins ++ custom
+  end
 
-          _ ->
-            false
-        end)
-        |> Enum.map(fn
-          %{type: :function, origin: origin, name: name, arity: arity} = completion ->
-            try do
-              {:docs_v1, _, _, _, _, _, functions} = Code.fetch_docs(Module.concat([origin]))
+  defp lowercase_string?(""), do: true
 
-              new_summary =
-                Enum.find_value(functions, fn
-                  {{:function, func_name, func_arity}, _, _,
-                   %{
-                     "en" => docs
-                   }, _} ->
-                    if to_string(func_name) == name && func_arity == arity do
-                      docs
-                    end
-
-                  _other ->
-                    false
-                end)
-
-              %{completion | summary: new_summary || completion.summary}
-            rescue
-              _e ->
-                completion
-            end
-
-          other ->
-            other
-        end)
-
-      first = behaviour |> Module.split() |> Enum.at(0)
-
-      custom =
-        for module <- module_store.by_behaviour[behaviour] || [],
-            mod_str = inspect(module),
-            !String.starts_with?(mod_str, "#{first}."),
-            Util.match_module?(mod_str, hint) do
-          {doc, _} = Introspection.get_module_docs_summary(module)
-
-          %{
-            type: :generic,
-            kind: :class,
-            label: mod_str,
-            insert_text: Util.trim_leading_for_insertion(hint, mod_str),
-            detail: "#{inspect(behaviour)}",
-            documentation: doc
-          }
-        end
-
-      builtins ++ custom
-    end
-
-    defp lowercase_string?(""), do: true
-
-    defp lowercase_string?(string) do
-      first = String.first(string)
-      String.downcase(first) == first
-    end
+  defp lowercase_string?(string) do
+    first = String.first(string)
+    String.downcase(first) == first
   end
 end
