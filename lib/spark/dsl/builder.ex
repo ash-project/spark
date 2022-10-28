275c2ee38e3a73e0b3641df71525a8108fad2f7a
Zach Daniel <zach@zachdaniel.dev>
2022-10-28 09:51:40 -0500

    improvement: add `handle_nested_builders/2`

--- lib/spark/dsl/builder.ex
+++ lib/spark/dsl/builder.ex
@@ -76,4 +76,56 @@ defmodule Spark.Dsl.Builder do
       end
     end
   end
+
+  @doc """
+  Handles nested values that may be `{:ok, result}` or `{:error, term}`, returning any errors and unwrapping any ok values
+
+  This allows users of builders to do things like:
+
+  ```elixir
+  dsl_state
+  |> Ash.Resource.Builder.add_new_action(:update, :publish,
+    changes: [
+      Ash.Resource.Builder.build_action_change(
+        Ash.Resource.Change.Builtins.set_attribute(:state, :published)
+      )
+    ]
+  )
+  ```
+
+  If your builder function calls `handle_nested_builders/2` with their input before building the thing its building.
+  """
+  def handle_nested_builders(opts, nested) do
+    Enum.reduce_while(nested, {:ok, opts}, fn nested, {:ok, opts} ->
+      case Keyword.get(opts, nested) do
+        nil ->
+          {:cont, {:ok, opts}}
+
+        values when is_list(values) ->
+          Enum.reduce_while(values, {:ok, []}, fn
+            {:ok, value}, {:ok, values} ->
+              {:cont, {:ok, [value | values]}}
+
+            {:error, error}, _ ->
+              {:halt, {:error, error}}
+
+            value, {:ok, values} ->
+              {:cont, {:ok, [value | values]}}
+          end)
+          |> case do
+            {:ok, values} -> {:cont, {:ok, Keyword.put(opts, nested, Enum.reverse(values))}}
+            other -> other
+          end
+
+        {:ok, value} ->
+          {:cont, {:ok, Keyword.put(opts, nested, value)}}
+
+        {:error, error} ->
+          {:halt, {:error, error}}
+
+        _value ->
+          {:cont, {:ok, opts}}
+      end
+    end)
+  end
 end
