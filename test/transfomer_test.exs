a3c031c353a12c3cd1ab268ebd10e98f7775c7a2
Barnabas Jovanovics <barnabas.jovanovics@gmail.com>
2022-12-07 13:51:29 +0100

    add transformer test

---
+++ test/transfomer_test.exs
@@ -0,0 +1,42 @@
+defmodule TransfomerTest do
+  use ExUnit.Case
+
+  test "build_entity respects auto_set_fields" do
+    defmodule Entity do
+      defstruct [:set_automatically, :set_manually]
+    end
+
+    defmodule Dsl do
+      @entity %Spark.Dsl.Entity{
+        name: :entity,
+        target: Entity,
+        schema: [
+          set_automatically: [
+            type: :boolean
+          ],
+          set_manually: [
+            type: :boolean
+          ]
+        ],
+        auto_set_fields: [set_automatically: true]
+      }
+
+      @section %Spark.Dsl.Section{
+        name: :section,
+        entities: [
+          @entity
+        ]
+      }
+
+      use Spark.Dsl.Extension, sections: [@section]
+      use Spark.Dsl, default_extensions: [extensions: Dsl]
+    end
+
+    {:ok, entity} = Spark.Dsl.Transformer.build_entity(Dsl, [:section], :entity, [])
+
+    IO.inspect(entity)
+
+    assert entity.set_automatically == true
+    assert entity.set_manually == nil
+  end
+end
