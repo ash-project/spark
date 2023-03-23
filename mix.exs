8b7d4ce4013f1105499a8323dda041ebc7704be8
Zach Daniel <zach@zachdaniel.dev>
2023-03-23 05:57:37 -0400

    improvement: update to nimble_options 1.0

--- mix.exs
+++ mix.exs
@@ -115,7 +115,7 @@ defmodule Spark.MixProject do
   # Run "mix help deps" to learn about dependencies.
   defp deps do
     [
-      {:nimble_options, "~> 0.5"},
+      {:nimble_options, "~> 1.0"},
       {:sourceror, "~> 0.1"},
       # {:dep_from_hexpm, "~> 0.3.0"},
       # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"}
