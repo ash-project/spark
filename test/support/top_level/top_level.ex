2fd930a61807f00d5d7c7061e660e494c8985f4a
Erika Rowland <accounts@erikarowland.com>
2023-08-07 14:16:22 -0700

    chore: update top_level test to meta-DSL

--- test/support/top_level/top_level.ex
+++ test/support/top_level/top_level.ex
@@ -2,83 +2,97 @@ defmodule Spark.Test.TopLevel do
   @moduledoc false
   defmodule Dsl do
     @moduledoc false
-    @atom %Spark.Dsl.Entity{
-      name: :atom,
-      target: Spark.Test.Atom,
-      args: [:name],
-      schema: [
-        name: [
-          type: :atom,
-          required: true
-        ]
-      ]
-    }
+    defmodule Impl do
+      @moduledoc false
+      use Spark
 
-    @step %Spark.Dsl.Entity{
-      name: :step,
-      target: Spark.Test.Step,
-      args: [:name],
-      recursive_as: :steps,
-      entities: [
-        steps: []
-      ],
-      schema: [
-        name: [
-          type: :atom,
-          required: true
-        ],
-        number: [
-          type: :integer
-        ]
-      ]
-    }
+      entity do
+        name(:atom)
+        target(Spark.Test.Atom)
+        args([:name])
 
-    @special_step %Spark.Dsl.Entity{
-      name: :special_step,
-      target: Spark.Test.Step,
-      args: [:name],
-      recursive_as: :steps,
-      entities: [
-        steps: []
-      ],
-      schema: [
-        name: [
-          type: :atom,
-          required: true
-        ],
-        number: [
-          type: :integer
-        ]
-      ]
-    }
+        schema do
+          field :name do
+            type(:atom)
+            required(true)
+          end
+        end
+      end
 
-    @nested_section %Spark.Dsl.Section{
-      name: :nested_section,
-      schema: [
-        bar: [
-          type: :integer,
-          doc: "Some documentation"
-        ]
-      ]
-    }
+      entity do
+        name(:step)
+        target(Spark.Test.Step)
+        args([:name])
+        recursive_as(:steps)
+        entities(steps: [])
 
-    @steps %Spark.Dsl.Section{
-      name: :steps,
-      top_level?: true,
-      sections: [
-        @nested_section
-      ],
-      schema: [
-        foo: [
-          type: :integer,
-          doc: "Some documentation"
-        ]
-      ],
-      entities: [@step, @special_step, @atom]
-    }
+        schema do
+          field :name do
+            type(:atom)
+            required(true)
+          end
+
+          field :number do
+            type(:integer)
+            required(false)
+          end
+        end
+      end
+
+      entity do
+        name(:special_step)
+        target(Spark.Test.Step)
+        args([:name])
+        recursive_as(:steps)
+
+        entities(steps: [])
+
+        schema do
+          field :name do
+            type(:atom)
+            required(true)
+          end
+
+          field :number do
+            type(:integer)
+            required(false)
+          end
+        end
+      end
+
+      section do
+        name(:nested_section)
+
+        schema do
+          field :bar do
+            type(:integer)
+            doc("Some documentation")
+            required(false)
+          end
+        end
+      end
+
+      section do
+        name(:steps)
+        sections([:nested_section])
+
+        schema do
+          field :foo do
+            type(:integer)
+            doc("Some documentation")
+            required(false)
+          end
+        end
+
+        entities([:step, :special_step, :atom])
+        top_level?(true)
+      end
+    end
+
+    sections = Spark.load(Impl, [:steps])
 
     use Spark.Dsl.Extension,
-      sections: [@steps]
+      sections: sections
   end
 
   use Spark.Dsl, default_extensions: [extensions: Dsl]
