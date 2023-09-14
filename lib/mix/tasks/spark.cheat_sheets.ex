8380afef96c760cad85f417cb13b07e328c98625
Zach Daniel <zach@zachdaniel.dev>
2023-09-14 16:54:09 -0400

    fix: don't remove DSL directory immediately after generating

--- lib/mix/tasks/spark.cheat_sheets.ex
+++ lib/mix/tasks/spark.cheat_sheets.ex
@@ -29,7 +29,5 @@ defmodule Mix.Tasks.Spark.CheatSheets do
       extension_name = Spark.Mix.Helpers.extension_name(extension, opts)
       File.write!("documentation/dsls/DSL:-#{extension_name}.cheatmd", cheat_sheet)
     end
-
-    File.rm_rf!("documentation/dsls")
   end
 end
