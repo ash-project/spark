869f8d82e5a6d10a8ad1394fc1f2649d346ec697
Zach Daniel <zach@zachdaniel.dev>
2023-12-21 07:43:32 -0500

    fix: keep subsection of target merged schema

--- lib/spark/options_helpers.ex
+++ lib/spark/options_helpers.ex
@@ -105,7 +105,7 @@ defmodule Spark.OptionsHelpers do
   def merge_schemas(left, right, section \\ nil) do
     new_right =
       Enum.map(right, fn {key, value} ->
-        {key, Keyword.put(value, :subsection, section)}
+        {key, Keyword.put_new(value, :subsection, section)}
       end)
 
     Keyword.merge(left, new_right)
