e145571009c31293163330bd8fc25ccfa8055813
Ali Makki <alimakki@gmail.com>
2024-02-13 10:57:08 -0500

    Update Sourceror  - bump version to `~> 1.0`

--- mix.exs
+++ mix.exs
@@ -130,7 +130,7 @@ defmodule Spark.MixProject do
   defp deps do
     [
       {:nimble_options, "~> 0.5 or ~> 1.0"},
-      {:sourceror, "~> 0.1"},
+      {:sourceror, "~> 1.0"},
       {:jason, "~> 1.4"},
       # {:dep_from_hexpm, "~> 0.3.0"},
       # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"}
