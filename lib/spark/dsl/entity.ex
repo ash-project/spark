fd444c9bbfc06a19ddcdf10317e1ecf330db44ed
James Harton <james@harton.dev>
2024-03-21 16:06:24 +1300

    improvement: Add `{:protocol, module}` as a supported type. (#79)

--- lib/spark/dsl/entity.ex
+++ lib/spark/dsl/entity.ex
@@ -254,7 +254,11 @@ defmodule Spark.Dsl.Entity do
          built <-
            struct(
              built,
-             Keyword.merge(Keyword.new(nested_entities), Keyword.new(more_nested_entities), fn _k, v1, v2 -> v1 ++ v2 end)
+             Keyword.merge(Keyword.new(nested_entities), Keyword.new(more_nested_entities), fn _k,
+                                                                                               v1,
+                                                                                               v2 ->
+               v1 ++ v2
+             end)
            ),
          {:ok, built} <- validate_singleton_entity_keys(built, singleton_entity_keys),
          {:ok, built} <- transform(transform, built) do
@@ -265,7 +269,7 @@ defmodule Spark.Dsl.Entity do
   defp fetch_single_argument_entities_from_opts(opts, nested_entity_definitions) do
     Enum.reduce_while(nested_entity_definitions, {:ok, opts, []}, fn
       {_key, entity_definitions}, {:ok, opts, more_nested_entities} ->
-      entity_definitions
+        entity_definitions
         |> Enum.filter(fn entity_definition -> Enum.count(entity_definition.args) == 1 end)
         |> Enum.reduce_while(
           {:ok, opts, more_nested_entities},
@@ -273,10 +277,23 @@ defmodule Spark.Dsl.Entity do
             values = Keyword.get_values(opts, entity_definition.name)
             opts = Keyword.delete(opts, entity_definition.name)
 
-            Enum.reduce_while(values, {:ok, opts, more_nested_entities}, fn single_arg, {:ok, opts, more_nested_entities} ->
-              case build(entity_definition, [{Enum.at(entity_definition.args, 0), single_arg}], []) do
+            Enum.reduce_while(values, {:ok, opts, more_nested_entities}, fn single_arg,
+                                                                            {:ok, opts,
+                                                                             more_nested_entities} ->
+              case build(
+                     entity_definition,
+                     [{Enum.at(entity_definition.args, 0), single_arg}],
+                     []
+                   ) do
                 {:ok, built} ->
-                  {:cont, {:ok, opts, Keyword.update(more_nested_entities, entity_definition.name, [built], &[built | &1])}}
+                  {:cont,
+                   {:ok, opts,
+                    Keyword.update(
+                      more_nested_entities,
+                      entity_definition.name,
+                      [built],
+                      &[built | &1]
+                    )}}
 
                 {:error, error} ->
                   {:halt, {:error, error}}
@@ -285,6 +302,7 @@ defmodule Spark.Dsl.Entity do
             |> case do
               {:ok, opts, more_nested_entities} ->
                 {:cont, {:ok, opts, more_nested_entities}}
+
               {:error, error} ->
                 {:halt, {:error, error}}
             end
@@ -293,6 +311,7 @@ defmodule Spark.Dsl.Entity do
         |> case do
           {:ok, opts, more_nested_entities} ->
             {:cont, {:ok, opts, more_nested_entities}}
+
           {:error, error} ->
             {:halt, {:error, error}}
         end
