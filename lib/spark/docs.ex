362f23493d9c3ee01476321dbee7580a663516f7
Zach Daniel <zach@zachdaniel.dev>
2025-01-27 17:42:42 -0500

    improvement: show DSL entities & sections in search

--- lib/spark/docs.ex
+++ lib/spark/docs.ex
@@ -28,6 +28,14 @@ defmodule Spark.Docs do
       end
 
     Enum.concat([
+      [
+        %{
+          anchor: anchor(schema_path),
+          body: section.describe,
+          title: title(schema_path),
+          type: "DSL"
+        }
+      ],
       schema_search_data(section.schema, schema_path),
       Enum.flat_map(section.entities, &entity_search_data(&1, path ++ [section.name]))
     ])
@@ -35,6 +43,14 @@ defmodule Spark.Docs do
 
   defp entity_search_data(entity, path) do
     Enum.concat([
+      [
+        %{
+          anchor: anchor(path ++ [entity.name]),
+          body: entity.describe,
+          title: title(path ++ [entity.name]),
+          type: "DSL"
+        }
+      ],
       schema_search_data(entity.schema, path ++ [entity.name]),
       Enum.flat_map(entity.entities, fn {_key, entities} ->
         Enum.flat_map(entities, fn entity ->
