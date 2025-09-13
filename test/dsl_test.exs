9a44167aeb06eac5a5e8cedf29b0a0ec158a8546
Josh Price <josh@alembic.com.au>
2025-09-13 17:11:19 +0200

    Raise helpful errors for DSL section inline and inline+block syntax
    Currently DSL sections throw confusing error messages for the following DSL declarations:
    
    1. Using inline keyword args instead of block syntax gives
    
    ```
    ** (Spark.Options.ValidationError) required :first_name option not found, received options: []
    ```
    
    2. Using inline and block syntax throws the following compile error
    
    ```
    error: undefined function section_name/2 (there is no such import)
    ```
    
    We now catch these errors and show helpful errors so that the user understands what went wrong and how to fix it. The new errors are as follows:
    
    1. Inline syntax
    ```
    Cannot use inline syntax for DSL section `personal_details`. Use block syntax: `personal_details do ... end`.
    ```
    
    2. Mixed inline and block syntax
    ```
    Cannot use both inline syntax and block syntax for DSL section `personal_details`. Use block syntax `personal_details do ... end`
    ```

--- test/dsl_test.exs
+++ test/dsl_test.exs
@@ -396,6 +396,36 @@ defmodule Spark.DslTest do
     end
   end
 
+  describe "DSL section syntax validation" do
+    test "mixed inline/block syntax should provide helpful error" do
+      assert_raise Spark.Error.DslError,
+                   ~r/Cannot use both inline syntax and block syntax.*personal_details/,
+                   fn ->
+                     defmodule MixedSyntax do
+                       @moduledoc false
+                       use Spark.Test.Contact
+
+                       personal_details first_name: "Luke" do
+                         last_name("Skywalker")
+                       end
+                     end
+                   end
+    end
+
+    test "inline syntax for DSL section should provide helpful error" do
+      assert_raise Spark.Error.DslError,
+                   ~r/Cannot use inline syntax for DSL section.*personal_details/,
+                   fn ->
+                     defmodule InlineSyntax do
+                       @moduledoc false
+                       use Spark.Test.Contact
+
+                       personal_details(first_name: "Luke", last_name: "Skywalker")
+                     end
+                   end
+    end
+  end
+
   describe "optional entity arguments" do
     test "optional arguments can be supplied either as arguments or as dsl options" do
       defmodule OptionEinstein do
